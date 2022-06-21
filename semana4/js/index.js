f6eae10b1eabf02aa743137ce0e6551e369dc91e
eavilac <erick.ac2911@gmail.com>
2022-06-20 22:51:14 -0500

    javascript

---
+++ semana4/js/index.js
@@ -0,0 +1,29 @@
+let numero = 10;
+
+console.log("Primer tipo ",numero);
+
+numero = "Pepe";
+
+console.log("Segundo tipo ",numero);
+
+const igv =0.18;
+console.log("Primer igv: ",igv);
+
+
+const primerNombre = "Erick";
+const primerApellido = "Avila";
+let edad = 24;
+let dias = 20;
+let estaVivo = true
+
+console.log("Concatenación");
+console.log(primerNombre+ " " + primerApellido + edad);
+
+// Sumar numeros
+console.log(edad+dias);
+
+// concatenar numeros
+console.log(String(edad)+String(dias));
+
+console.log("Bool", estaVivo);
+console.log("String", String(estaVivo));
